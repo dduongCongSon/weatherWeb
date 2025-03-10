e35c17e3348d3d55dce066f708e72032b2fd2c54
dduongCongSon <sonduongcong8@gmail.com>
2025-03-11 06:55:24 +0700

    fix: skeleton in home page

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../components/ui/button";
+import { RefreshCw } from "lucide-react";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import clsx from "clsx";
+import WeatherSkeleton from "@/components/loading-skeleton";
 
 const WeatherDashboard = () => {
-  return (
-    <div>WeatherDashboard</div>
-  )
-}
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
+  console.log("coordinates", coordinates);
 
-export default WeatherDashboard
+  const hanldeRefresh = () => {
+    getLocation();
+    if (coordinates) {
+    }
+  };
+
+
+  if (locationLoading){
+    return <WeatherSkeleton/>
+  }
+    return (
+      <div className="space-y-4">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            // onClick={hanldeRefresh}
+            // disabled={}
+          >
+            <RefreshCw className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    );
+};
+
+export default WeatherDashboard;
